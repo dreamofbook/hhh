92bd9b984e46dfe3bea17fa3222469c8ae05b282
dreamofbook <redich.m4@gmail.com>
2025-04-25 21:55:09 +0300

    червяка сделал
    Нужно исправить это)

--- src/components/OtherComponents/Card/Card.jsx
+++ src/components/OtherComponents/Card/Card.jsx
@@ -3,7 +3,9 @@ import './card.css'
 
 const Card = ({image, title, body, ...props}) => {
 	return (
-		<div className={'card'}>
+		<div className={'card'}
+			onMouseEnter={(e) => console.log(e.target.zIndex)}
+		>
 			<div className={'card-content'}>
 				<div className={'card-image'}><img src={image} alt={''}/></div>
 				<div className={'card-header text'}>{title}</div>
--- src/components/OtherComponents/Feedback/FeedbackCarusel.jsx
+++ src/components/OtherComponents/Feedback/FeedbackCarusel.jsx
@@ -3,15 +3,35 @@ import FeedbackCard from "./FeedbackCard.jsx";
 import './styles/carusel.css';
 
 const FeedbackCarusel = () => {
+
+	const Scroll = () => {
+		var Cards = document.querySelectorAll('.feedbackCard');
+		const last = Cards.length - 1;
+		const parent = document.querySelector('.carusel');
+
+		Cards.forEach((card, index) => {
+				let position = card.getBoundingClientRect()
+				card.style.transform += `translateX(${position.width + 10}px)`;
+		})
+		
+		parent.prepend(Cards[last]);
+		console.log('effect used')
+	}
+
+	let interval = setInterval(Scroll, 4000)
+	React.useEffect(() => {
+		interval;
+	})
+
 	return (
 		<div className={"carusel"}>
-			<div className={"blackOut1"}></div>
-			<FeedbackCard body={'Сервис работает нормально, все крутится, работает и пишет!'} name={'Вячеслав'} position={'Полный долбаеб'}/>
-			<FeedbackCard body={'Сервис работает нормально, все крутится, работает и пишет!'} name={'Вячеслав'} position={'Полный долбаеб'}/>
-			<FeedbackCard body={'Сервис работает нормально, все крутится, работает и пишет!'} name={'Вячеслав'} position={'Полный долбаеб'}/>
-			<FeedbackCard body={'Сервис работает нормально, все крутится, работает и пишет!'} name={'Вячеслав'} position={'Полный долбаеб'}/>
-			<FeedbackCard body={'Сервис работает нормально, все крутится, работает и пишет!'} name={'Вячеслав'} position={'Полный долбаеб'}/>
-			<div className={"blackOut2"}></div>
+			{/*<div className={"blackOut1"}></div>*/}
+			<FeedbackCard body={'зуй пизда член 1'} name={'Вячеслав'} position={'Полный долбаеб'}/>
+			<FeedbackCard body={'зуй пизда член 2'} name={'Вячеслав'} position={'Полный долбаеб'}/>
+			<FeedbackCard body={'зуй пизда член 3'} name={'Вячеслав'} position={'Полный долбаеб'}/>
+			<FeedbackCard body={'зуй пизда член 4'} name={'Вячеслав'} position={'Полный долбаеб'}/>
+			<FeedbackCard body={'зуй пизда член 5'} name={'Вячеслав'} position={'Полный долбаеб'}/>
+			{/*<div className={"blackOut2"}></div>*/}
 		</div>
 	);
 };
